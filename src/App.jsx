a06bea8fdc0deeb7e2ff3886fd22e2400e565b2f
Akilan <akilanlakshmi2003@gmail.com>
2023-12-19 14:12:37 +0530

    Updated App.jsx. Added react-router-dom, home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./pages/Home.jsx"
 function App() {
 
   return (
-      <div className='bg-black flex justify-center items-center w-full h-screen text-white text-3xl tracking-wider'>
-        This is a starter kit. Edit <span className='text-blue-300 px-2'>App.jsx</span> for your project.
-      </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='bg-black flex justify-center items-center w-full h-screen text-white text-3xl tracking-wider'>
+            This is a starter kit. Edit <span className='flex justify-center items-center bg-gray-900 text-xl text-blue-300 mx-2 px-2 rounded-xl h-8 cursor-pointer'>src/pages/Home.jsx</span> for your project.
+        </div>
+    )
+}
+
+export default Home
